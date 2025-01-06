728a528350299a3e7f246671154d2a406e9bfea9
Muzna-Dawar <muznadawar@gmail.com>
2025-01-06 14:57:28 +0500

    new update

---
+++ calculator.js
@@ -0,0 +1,32 @@
+function add() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    document.getElementById("result").innerHTML = Number(num1)+Number(num2);
+}
+
+
+function subtract() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    document.getElementById("result").innerHTML = Number(num1)-Number(num2);
+}
+
+
+function multiply() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    document.getElementById("result").innerHTML = Number(num1)*Number(num2);
+}
+
+
+function divide() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    document.getElementById("result").innerHTML = Number(num1)/Number(num2);
+}
+function reset() {
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("result").innerHTML = "";
+}
+
